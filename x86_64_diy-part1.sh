e8d86e1cf6e71b767647d92ffa73c9a588252d39
bingbinghj <77175750+bingbinghj@users.noreply.github.com>
2026-05-05 18:53:47 +0800

    Update and rename diy-part1.sh to x86_64_diy-part1.sh

--- x86_64_diy-part1.sh
+++ x86_64_diy-part1.sh
@@ -14,5 +14,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+#small-package
+echo 'src-git kenzok8 https://github.com/kenzok8/small-package.git' >> feeds.conf.default
